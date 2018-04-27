8f1353abd66be318ddc33f5e82556f9bb04dfcac
Josh Jancula <josh@jancula.com>
2018-04-27 22:17:37 +0000

    style

--- public/initializeContact.js
+++ public/initializeContact.js
@@ -2,7 +2,7 @@
   $(document).ready(function() {
     var elem = document.querySelector('.modal');
 var instance = new M.Modal(elem, {});
-    $('.modal').modal();
+    // $('.modal').modal();
    
 });
    
@@ -10,6 +10,6 @@ var instance = new M.Modal(elem, {});
     $(document).ready(function() {
     var elem = document.querySelector('.sidenav');
 var instance = new M.Sidenav(elem, {});
-    $('.sidenav').sidenav();
+    // $('.sidenav').sidenav();
 
 });
